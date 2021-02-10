3b26e7731b22463e9ad88b801875f976fd8f3557
BuyukCem <cembuyuk7@gmail.com>
2021-02-10 17:32:04 +0100

    first Commit

---
+++ _vti_cnf/animate.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|Hasan Buyuk
+vti_modifiedby:SR|Hasan Buyuk
+vti_timecreated:TR|06 Feb 2006 15:35:22 -0000
+vti_timelastmodified:TR|06 Feb 2006 15:35:22 -0000
+vti_cacheddtm:TX|06 Feb 2006 15:35:22 -0000
+vti_filesize:IR|14261
+vti_extenderversion:SR|4.0.2.8912
+vti_backlinkinfo:VX|sommaire.htm les_bandes_verticales.htm titre.htm stores_venitien\\ alu.htm stores__bateaux.htm corps.htm stores_interieures\\ acceuil.htm stores_banne.htm porte\\ de\\ garage.htm
